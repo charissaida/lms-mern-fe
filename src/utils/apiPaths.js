68cabbda84c188938af35e8f139034a846432b24
charissaida <charis.saida@gmail.com>
2025-09-06 22:51:58 +0700

    update: deploy

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://eduplant-be.vercel.app/";
+export const BASE_URL = "http://203.194.114.43:5000/";
 // export const BASE_URL = "http://localhost:8000";
 
 export const API_PATHS = {
