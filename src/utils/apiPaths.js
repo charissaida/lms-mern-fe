1426c29afc121c094f8223cd1e6f03ed0db274cc
charissaida <charis.saida@gmail.com>
2025-09-07 00:08:12 +0700

    update: deploy

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://203.194.114.43:5000/";
+export const BASE_URL = "https://eduplant.duckdns.org/";
 // export const BASE_URL = "http://localhost:8000";
 
 export const API_PATHS = {
